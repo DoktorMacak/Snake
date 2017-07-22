4fb5bf4e7fd79fdcdcb263450c82d21262b872bc
DoktorMacak <momciloviclazar@gmail.com>
2017-07-22 19:31:21 +0200

    Initial commit

---
+++ android/src/com/semblergames/snake/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.semblergames.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.semblergames.snake.main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new main(), config);
+	}
+}
