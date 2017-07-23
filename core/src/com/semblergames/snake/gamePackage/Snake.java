a6a91a946723686a039f8beb9f5e0740a46a1eb2
DoktorMacak <momciloviclazar@gmail.com>
2017-07-23 12:04:29 +0200

    123

--- core/src/com/semblergames/snake/gamePackage/Snake.java
+++ core/src/com/semblergames/snake/gamePackage/Snake.java
@@ -14,12 +14,16 @@ public class Snake {
      *current snake's moving direction
      */
     private Direction direction;
+    /**
+     *is snake growing
+     */
+    private boolean grow = false;
 
     public Snake(int initialLength, Direction initialDirection, int initialX, int initialY){
         this.direction = initialDirection;
         switch (direction){
             case left: for(int i = 0; i < initialLength; i++)
-                    segments.add(new Segment(initialX + i, initialY));
+                segments.add(new Segment(initialX + i, initialY));
             case right: for(int i = 0; i < initialLength; i++)
                 segments.add(new Segment(initialX + i, initialY));
             case up: for(int i = 0; i < initialLength; i++)
@@ -29,7 +33,26 @@ public class Snake {
         }
     }
 
+    public void update(){
+        int x = segments.get(segments.size()).getX();
+        int y = segments.get(segments.size()).getY();
+        switch (direction){
+            case up: y++;
+            case down: y--;
+            case left: x--;
+            case right: x++;
+        }
+        segments.add(new Segment(x,y));
+        segments.remove(0);
+    }
 
+    public void grow(){
+        grow = true;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
 
     public List<Segment> getSegments() {
         return segments;
