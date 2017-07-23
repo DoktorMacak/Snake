f9dc327b4d7f1385407ae77ecdf1e87efbc9c100
DoktorMacak <momciloviclazar@gmail.com>
2017-07-23 17:15:51 +0200

    123

--- core/src/com/semblergames/snake/gamePackage/PlayState.java
+++ core/src/com/semblergames/snake/gamePackage/PlayState.java
@@ -2,10 +2,16 @@ package com.semblergames.snake.gamePackage;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.semblergames.snake.utilities.Direction;
 
 public class PlayState extends GameState {
 
     PlayingRegion pl;
+
+    Snake snake;
+
+    private float speed;
+    private float time;
 
     public PlayState() {
     }
@@ -14,6 +20,9 @@ public class PlayState extends GameState {
     public void init() {
         pl = new PlayingRegion();
         pl.setRelativePos(0,0);
+        snake = new Snake(3, Direction.up, 5, 3);
+        speed = 0.8f;
+        time = 0f;
     }
 
     @Override
@@ -22,11 +31,17 @@ public class PlayState extends GameState {
     }
 
     @Override
-    public void render(SpriteBatch batch, ShapeRenderer renderer, float alpha) {
+    public void render(SpriteBatch batch, ShapeRenderer renderer, float alpha, float delta) {
         //ShapeRenderer.ShapeType.Filled
         renderer.setAutoShapeType(true);
         renderer.begin(ShapeRenderer.ShapeType.Filled);
+        time+=delta;
+        snake.draw(renderer);
         pl.drawRegion(renderer);
+        if (time > speed){
+            snake.update();
+            time = 0f;
+        }
         renderer.end();
     }
 
@@ -42,7 +57,14 @@ public class PlayState extends GameState {
 
     @Override
     public void touchUp(int x, int y) {
-
+        if (Math.abs(x) > Math.abs(y)){
+            if(x > 0) snake.setDirection(Direction.left);
+            else snake.setDirection(Direction.right);
+        }else{
+            if(y > 0) snake.setDirection(Direction.down);
+            else snake.setDirection(Direction.up);
+        }
+        //snake.update();
     }
 
     @Override
